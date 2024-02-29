cf4dcf11dc64844c4484129ce4d0b6cd46dc84c6
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-03-01 01:59:10 +0400

    inital commit

---
+++ backend/chatapi/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Add other URL patterns for your API views
+]
